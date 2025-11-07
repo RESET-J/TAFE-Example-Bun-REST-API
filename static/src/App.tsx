a3349c2cebe725d33288eea2ab16b94152f74084
RESET-J <20103206@tafe.wa.edu.au>
2025-11-07 19:51:15 +0800

    feat: Implimented main / header

--- static/src/App.tsx
+++ static/src/App.tsx
@@ -5,13 +5,33 @@ import "./index.css";
 import logo from "./logo.svg";
 import reactLogo from "./react.svg";
 
+import Header from "./components/Header";
+
 // pages
 import GetAllPage from "./pages/GetAllPage";
 
 export function App() {
   return (<>
-    <>Hello, World!</>
-    <GetAllPage />
+    <Header />
+    <div className="container d-flex flex-column align-items-center">
+      <div className="d-flex p-4 gap-3">
+        <button className="btn btn-primary px-5">
+          GET
+        </button>
+         <button className="btn btn-primary px-5">
+          GET
+        </button>
+         <button className="btn btn-primary px-5">
+          POST
+        </button>
+         <button className="btn btn-primary px-5">
+          PATCH
+        </button>
+         <button className="btn btn-primary px-5">
+          DELETE
+        </button>
+      </div>
+    </div>
   </>
   );
 }
--- static/src/components/Header.js
+++ static/src/components/Header.js
@@ -1,16 +1,15 @@
 import React from "react";
 
 export default function Header() {
+    const styles = {
+        backgroundColor: '#213ABF',
+    }
+
     return (
         <header>
-            <nav className="navbar bg-body-tertiary">
+            <nav className="navbar" style={styles}>
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="#">Navbar</a>
-                </div>
-            </nav>
-            <nav className="navbar bg-body-tertiary">
-                <div className="container-fluid">
-                    <span className="navbar-brand mb-0 h1">Navbar</span>
+                    <a style={{ color: 'white' }} className="navbar-brand" href="#">Example Bun REST API</a>
                 </div>
             </nav>
         </header>
